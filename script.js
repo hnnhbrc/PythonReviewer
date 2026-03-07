caaaf5cb6c1555f49a49df7f69883ca6611fbd7e
devi <abrecea.hvp@gmail.com>
2026-03-08 02:39:30 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -10,27 +10,16 @@ document.getElementById("questionNumber").innerHTML =
 "Question " + (currentQuestion+1) + " of " + questions.length
 
 document.getElementById("questionText").innerHTML = q.question || ""
-
 document.getElementById("codeBlock").innerText = q.code || ""
 
-
-/* IMAGE */
-
 if(q.image){
-
 document.getElementById("questionImage").innerHTML =
 `<img src="${q.image}" class="question-img">`
-
 }else{
-
 document.getElementById("questionImage").innerHTML=""
 }
 
-
 let html = ""
-
-
-/* MULTI DROPDOWN */
 
 if(q.type === "multi-dropdown"){
 
@@ -39,13 +28,10 @@ q.subquestions.forEach((sub,i)=>{
 html += `<p>${sub.text}</p>`
 
 html += `<select onchange="saveDropdown(${i},this.value)">`
-
 html += `<option value="">Select answer</option>`
 
 sub.choices.forEach(choice=>{
-
 html += `<option value="${choice}">${choice}</option>`
-
 })
 
 html += `</select><br><br>`
@@ -53,9 +39,6 @@ html += `</select><br><br>`
 })
 
 }
-
-
-/* MULTIPLE CHOICE */
 
 if(q.type === "mcq"){
 
@@ -72,9 +55,6 @@ ${choice}
 
 }
 
-
-/* MULTI SELECT */
-
 if(q.type === "multi-select"){
 
 q.choices.forEach((choice,i)=>{
@@ -90,9 +70,6 @@ ${choice}
 
 }
 
-
-/* TRUE FALSE */
-
 if(q.type === "truefalse-group"){
 
 q.subquestions.forEach((sub,i)=>{
@@ -100,8 +77,8 @@ q.subquestions.forEach((sub,i)=>{
 html += `<p>${sub.text}</p>`
 
 html += `
-<label><input type="radio" name="tf${i}" onchange="saveTF(${i},'True')"> True</label>
-<label><input type="radio" name="tf${i}" onchange="saveTF(${i},'False')"> False</label>
+<label><input type="radio" name="tf${i}" value="True" onchange="saveTF(${i},'True')"> True</label>
+<label><input type="radio" name="tf${i}" value="False" onchange="saveTF(${i},'False')"> False</label>
 <br><br>
 `
 
@@ -109,14 +86,11 @@ html += `
 
 }
 
-
 document.getElementById("answers").innerHTML = html
 
 updateProgress()
 
 }
-
-
 
 function saveAnswer(ans){
 userAnswers[currentQuestion] = ans
@@ -153,17 +127,125 @@ userAnswers[currentQuestion][index] = value
 
 }
 
+/* CHECK ANSWERS */
 
+function checkAnswer(){
+
+let q = questions[currentQuestion]
+let container = document.getElementById("answers")
+
+container.querySelectorAll(".correct,.wrong").forEach(el=>{
+el.classList.remove("correct","wrong")
+})
+
+if(q.type === "multi-dropdown"){
+
+let selects = container.querySelectorAll("select")
+
+selects.forEach((sel,i)=>{
+
+if(sel.value === q.subquestions[i].answer)
+sel.classList.add("correct")
+else
+sel.classList.add("wrong")
+
+})
+
+}
+
+if(q.type === "mcq"){
+
+let radios = container.querySelectorAll("input[type=radio]")
+
+radios.forEach((r,i)=>{
+
+let label = r.parentElement
+
+if(i === q.answer)
+label.classList.add("correct")
+
+if(r.checked && i !== q.answer)
+label.classList.add("wrong")
+
+})
+
+}
+
+if(q.type === "multi-select"){
+
+let checks = container.querySelectorAll("input[type=checkbox]")
+
+checks.forEach((c,i)=>{
+
+let label = c.parentElement
+
+if(q.answers.includes(i))
+label.classList.add("correct")
+
+if(c.checked && !q.answers.includes(i))
+label.classList.add("wrong")
+
+})
+
+}
+
+if(q.type === "truefalse-group"){
+
+q.subquestions.forEach((sub,i)=>{
+
+let radios = container.querySelectorAll(`input[name="tf${i}"]`)
+
+radios.forEach(r=>{
+
+let label = r.parentElement
+
+if(r.value === sub.answer)
+label.classList.add("correct")
+
+if(r.checked && r.value !== sub.answer)
+label.classList.add("wrong")
+
+})
+
+})
+
+}
+
+}
+
+/* TRY AGAIN */
+
+function resetQuestion(){
+
+let container = document.getElementById("answers")
+
+container.querySelectorAll(".correct,.wrong").forEach(el=>{
+el.classList.remove("correct","wrong")
+})
+
+container.querySelectorAll("select").forEach(sel=>{
+sel.selectedIndex = 0
+})
+
+container.querySelectorAll("input[type=radio]").forEach(r=>{
+r.checked = false
+})
+
+container.querySelectorAll("input[type=checkbox]").forEach(c=>{
+c.checked = false
+})
+
+userAnswers[currentQuestion] = null
+
+}
 
 /* NAVIGATION */
 
 document.getElementById("nextBtn").onclick = () => {
 
 if(currentQuestion < questions.length-1){
-
 currentQuestion++
 loadQuestion()
-
 }
 
 }
@@ -171,55 +253,40 @@ loadQuestion()
 document.getElementById("prevBtn").onclick = () => {
 
 if(currentQuestion > 0){
-
 currentQuestion--
 loadQuestion()
-
 }
 
 }
 
-
-
-/* PROGRESS BAR */
+/* PROGRESS */
 
 function updateProgress(){
 
 let percent = ((currentQuestion+1)/questions.length)*100
-
 document.getElementById("progressFill").style.width = percent + "%"
 
 }
 
-
-
-/* OPTIONAL SHUFFLE */
+/* SHUFFLE */
 
 function shuffleQuestions(){
 
 questions.sort(()=>Math.random()-0.5)
 
 currentQuestion = 0
-
 loadQuestion()
 
 }
-
-
-
-/* RESET ORDER */
 
 function resetOrder(){
 
 questions = [...originalQuestions]
 
 currentQuestion = 0
-
 loadQuestion()
 
 }
-
-
 
 /* DARK MODE */
 
@@ -229,16 +296,11 @@ darkBtn.addEventListener("click", function(){
 
 document.body.classList.toggle("dark-mode")
 
-if(document.body.classList.contains("dark-mode")){
+if(document.body.classList.contains("dark-mode"))
 darkBtn.textContent = "Light Mode"
-}else{
+else
 darkBtn.textContent = "Dark Mode"
-}
 
 })
-
-
-
-/* START */
 
 loadQuestion()
